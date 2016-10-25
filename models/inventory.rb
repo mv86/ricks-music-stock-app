2662a71455a04d6f889e02980003c5bf5452d21e
Max <mveasey87@gmail.com>
2016-10-25 11:51:13 +0100

    added stock level accessor to Inventory class, test passed

--- models/inventory.rb
+++ models/inventory.rb
@@ -2,8 +2,17 @@
 
 class Inventory
 
-  def stock_accesor
-    
+  def self.stock_level_accessor(stock)
+    case stock.quantity
+    when 1..5
+      return 'Very Low'
+    when 6..10
+      return 'Low'
+    when 11..20
+      return 'Good'
+    when 21..99
+      return 'High'
+    end
   end
 
 end
--- specs/inventory_specs.rb
+++ specs/inventory_specs.rb
@@ -1,11 +1,17 @@
 require 'minitest/autorun'
-require 'minitest/pg'
+require 'minitest/rg'
 require_relative '../models/inventory'
+require_relative '../models/stock'
 
 class TestInventory < MiniTest::Test
 
   def setup
-    
+    @stock1 = Stock.new( {'quantity' => 2, 'buy_price' => 4.20, 'sell_price' => 10.50} )
   end
-  
+
+  def test_stock_level_accessor
+    stock_level =  Inventory.stock_level_accessor(@stock1)
+    assert_equal('Very Low', stock_level)
+  end
+
 end
