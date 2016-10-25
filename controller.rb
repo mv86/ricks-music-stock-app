53639b3370103425e2e8dd200b6b28aff0810134
Max <mveasey87@gmail.com>
2016-10-25 18:19:10 +0100

    added css & home.erb

--- controller.rb
+++ controller.rb
@@ -3,3 +3,7 @@ require 'sinatra/contrib/all'
 require_relative 'controllers/album_controller'
 require_relative 'controllers/artist_controller'
 require_relative 'controllers/stock_controller'
+
+get '/' do
+  erb(:home)
+end
